55850a5e7cc0496e15e8df229a02447b1b80a336
Sagiv Ofek <sagiv4@gmail.com>
2015-06-09 02:04:34 -0400

    Pricing

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
   });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 });
