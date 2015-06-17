1a2c767cfc39a12e221ea27ae7f13fcbdd561400
Sagiv Ofek <sagiv4@gmail.com>
2015-06-16 21:48:52 -0400

    Refactor lib

--- app/controllers/home_controller.js
+++ app/controllers/home_controller.js
@@ -1,6 +1,10 @@
 var tesseract = require('node-tesseract');
 
 exports.index = function(req, res){
+  res.render('home', {text: 'hello'});
+}
+
+exports.test = function(req, res){
   tesseract.process(__dirname + '/1.png', function(err, text) {
     if(err) console.error(err);
     else {
