0463b293a07a67d196d08092dbca4f27be54b6fe
Sagiv Ofek <sagiv4@gmail.com>
2015-07-27 22:41:48 -0400

    flash

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -6,22 +6,25 @@ const User = mongoose.model('User');
 var crypt = require('dead-simple-crypt', secrets.CRYPT_KEY);
 
 exports.login = function(req, res){
-  console.log(req.body);
+  console.log('in login');
   User.findOne({email: req.body.email, password: crypt.encrypt(req.body.password)}, function(err, user){
     if (user){
       res.cookie('t', user.token, {expires: new Date(Date.now() + 30 * 24 * 3600000) }); //30 days
-      res.render('user/dashboard', {alert: {type: 'success', msg: 'Welcome again.'} })
-    } else {res.alert = {type: 'danger', msg: 'User not found!'}; res.redirect('/dashboard'); }
+      res.redirect('/dashboard?flash=success--Welcome back!--')
+    } else {
+      console.log('in else');
+      res.redirect('/dashboard?flash=danger--Wrong email/password. Please try again.--')
+    }
   });
 }
 
 exports.register = function(req, res){
   let token = crypt.gui();
   User.create({email: req.body.email, password: crypt.encrypt(req.body.password), token: token}, function(err, user){
-    if (err) res.render('user/dashboard', {alert: {type: 'danger', msg: 'Oops, something bad happened. Please try again.'} });
+    if (err) res.redirect('dashboard?flash=danger--Oops, something bad happened. Please try again.--');
     if (user){
       res.cookie('t', token, {expires: new Date(Date.now() + 30 * 24 * 3600000) }); //30 days
-      res.render('user/dashboard', {alert: {type: 'success', msg: 'Welcome! Please check your email to verify the account.'} })
+      res.redirect('dashboard?flash=success--Welcome! Please check your email to verify the account.--')
     }
   });
 }
